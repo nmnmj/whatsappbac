a58f108dd4b6a24c4e9f60e658b5de6ecc415982
nmnmj <nmnjay@gmail.com>
2023-03-30 15:18:20 +0530

    n

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ import web from './routes/web.js'
 import connectDB from './db/connectmongo.js'
 dotenv.config()
 const app = express()
-app.use(cors())
+app.use(cors({
+    origin:"http://localhost:3000/"
+}))
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
